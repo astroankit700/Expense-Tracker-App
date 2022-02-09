144034176a0d5f5538809cf1714f80f4fd885a5d
astroankit700 <ankitagrawal700aa@gmail.com>
2022-02-09 15:44:47 +0530

    added collapsing expense form functionality

--- src/components/NewExpense/NewExpense.jsx
+++ src/components/NewExpense/NewExpense.jsx
@@ -1,20 +1,42 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import ExpenseForm from './ExpenseForm';
 import './NewExpense.css';
 
 const NewExpense = (props) => {
+    const [isEditing, setIsEditing] = useState(false);
+
     const saveExpenseDataHandler = enteredExpenseData => {
         const expenseData = {
             id: Math.random().toString(),
             ...enteredExpenseData
         }
         props.onAddExpense(expenseData);
+        setIsEditing(false)
+    }
+
+    const stopEditingHandler = () =>{
+        setIsEditing(false)
+    }
+
+    const startEditingHandler = () =>{
+        setIsEditing(true)
     }
 
     return (
-        <div className='new-expense'>
-            <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+    <div className='new-expense'>
+        {!isEditing && (
+            <button onClick={startEditingHandler}>Add New Expense</button>
+        )}
+        {isEditing && (
+            <ExpenseForm
+            onSaveExpenseData={saveExpenseDataHandler}
+            onCancel={stopEditingHandler}
+            />
+        )}
+        {/* {isEditing ? 
+            <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} onCancel={stopEditingHandler} />
+         : <button onClick={startEditingHandler}>Add Expense</button>} */}
         </div>
     );
 };
